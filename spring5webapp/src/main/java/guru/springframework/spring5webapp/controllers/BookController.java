22e09b95a5c43eaa454e093a55b8d5ad1bb770e1
hldrmaia <hldrmaia@gmail.com>
2020-06-09 00:35:44 -0300

    Adding the package controllers, and class BookController

---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,36 @@
+package guru.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+
+@Controller
+public class BookController {
+	
+	/* Using the method getBooks to access data and return the model to view
+	 * 
+	 *  1- Create the package controllers
+	 *  2- Create the class BookController
+	 *  3- Add the Stereotype @Controller to the class
+	 *  4- Create the method getBooks, which receive the model to access the repository, calling the method findAll and return it to the view
+	 *  5- Add the Stereotype @RequestMapping, which maps /book to the URL
+	 *  
+	 */
+	
+	private final BookRepository bookRepository;
+	
+	public BookController(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+
+	@RequestMapping("/books")
+	public String getBooks(Model model) {
+		
+		model.addAttribute("books", bookRepository.findAll());
+		
+		return "books";
+	}
+
+}
